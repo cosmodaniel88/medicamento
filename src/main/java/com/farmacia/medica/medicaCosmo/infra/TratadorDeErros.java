8eb55178e4f5010d70ed2bb0e7eeda3d61410e31
daniel_cosmo <cosmodaniel88@gmail.com>
2024-07-07 09:26:37 -0400

    tratando erros

--- src/main/java/com/farmacia/medica/medicaCosmo/infra/TratadorDeErros.java
+++ src/main/java/com/farmacia/medica/medicaCosmo/infra/TratadorDeErros.java
@@ -1,6 +1,8 @@
 package com.farmacia.medica.medicaCosmo.infra;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,9 +10,23 @@ import jakarta.persistence.EntityNotFoundException;
 
 @RestControllerAdvice // caso ocorra algum erro, o spring buscará o tratamento diretamente aqui.
 public class TratadorDeErros {
-	
+
 	@ExceptionHandler(EntityNotFoundException.class)
 	public ResponseEntity<?> tratador404() {
 		return ResponseEntity.notFound().build();
 	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> tratador400(MethodArgumentNotValidException ex) {
+		var erros = ex.getFieldErrors();
+		return ResponseEntity.badRequest().body(erros.stream().map(DadosDoErro::new).toList());
+
+	}
+
+	public record DadosDoErro(String field, String message) {
+		public DadosDoErro(FieldError erro) {
+			this(erro.getField(), erro.getDefaultMessage());
+		}
+	}
+
 }
