f13f3b7d46bec4ccdecc8d6dffe16a3af9c6f575
daniel_cosmo <cosmodaniel88@gmail.com>
2024-07-14 11:32:38 -0400

    Validando o token - front

---
+++ src/main/java/com/farmacia/medica/medicaCosmo/infra/SecurityFilter.java
@@ -0,0 +1,44 @@
+package com.farmacia.medica.medicaCosmo.infra;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/*Nesta classe será implementada a lógica de validação do token
+ * Nós primeiramente geramos um token, após a requisição do front
+ * após isso, devolvemos esse token para o front
+ * agora, o front nos enviará o toke gerado para  implementarmos de fato a lógica da validação
+ * */
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		var tokenJwt = recuperarToken(request);
+		
+		System.out.println(tokenJwt);
+		
+		filterChain.doFilter(request, response);
+
+	}
+/*Esse método vai pegar o token passado no cabeçalho e 
+ * o retornará como string*/
+	private String recuperarToken(HttpServletRequest request) {
+		var authoHead = request.getHeader("Authorization");
+		if (authoHead == null) {
+			throw new RuntimeException("Token não enviado");
+		}
+		
+		return authoHead;
+	}
+
+}
